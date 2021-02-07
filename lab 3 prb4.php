4e58a71c623e71d0616249297b1570c8b32b5353
sadia-ety <78290919+sadia-ety@users.noreply.github.com>
2021-02-07 16:37:08 +0600

    Add files via upload

---
+++ lab 3 prb4.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>problem1</title>
+  </head>
+  <body>
+    <?php
+    $name = "mina";
+    if($name == "raju" || $name == "mina" || $name == "mithu")
+     {
+        echo "<b>hello</b>";
+     }
+
+
+    else echo "you are not recognized";
+
+     ?>
+
+  </body>
+</html>
---
+++ lab 3.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>problem1</title>
+  </head>
+  <body>
+    <span>the quick brown</span>
+    <?php
+echo "<b>fox</b>";
+    ?>
+    <span>jumps over a lazy</span>
+    <?php
+echo "<b>dog</b>";
+    ?>
+
+
+  </body>
+</html>
